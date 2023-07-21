4a6abf7a7d7d276cfa5c38178de3a980d9fbab70
TarasLys <opros0609@gmail.com>
2023-07-21 22:15:12 +0300

    ver

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+
+$('.autoplay').slick({
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dotsClass: "my-dots",
+    autoplay: true,
+    autoplaySpeed: 200000,
+    nextArrow: '<button type="button" class="slick-next"><img src="./image/ep_arrow-right.svg" alt="next"></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="./image/ep_arrow-left.svg" alt="prev"></button>'
+});
