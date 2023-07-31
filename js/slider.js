c0d6adcf330a54b3df169d837cf385d52f8bbd79
TarasLys <opros0609@gmail.com>
2023-07-31 17:40:20 +0300

    кнопки

--- js/slider.js
+++ js/slider.js
@@ -5,7 +5,10 @@ $('.autoplay').slick({
     slidesToScroll: 1,
     dotsClass: "my-dots",
     autoplay: true,
-    autoplaySpeed: 200000,
-    nextArrow: '<button type="button" class="slick-next"><img src="./image/ep_arrow-right.svg" alt="next"></button>',
-    prevArrow: '<button type="button" class="slick-prev"><img src="./image/ep_arrow-left.svg" alt="prev"></button>'
+    autoplaySpeed: 2000,
+    nextArrow: '<button type="button" class="slick-next"><img src="./image/right.png" alt="next"></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="./image/left.png" alt="prev"></button>'
 });
+
+// nextArrow: '<button type="button" class="slick-next"><img src="./image/ep_arrow-right.svg" alt="next"></button>',
+//     prevArrow: '<button type="button" class="slick-prev"><img src="./image/ep_arrow-left.svg" alt="prev"></button>'
